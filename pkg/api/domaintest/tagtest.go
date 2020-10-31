9eef5786158d90ae433f8596a8732f83efa03ca0
DavudSafarli <davudseferli@gmail.com>
2020-10-31 13:19:13 +0400

    test(tag): create behaviour test for TagRepository
    create behaviour test for TagRepository that tests if its methods' correctness

---
+++ pkg/api/domaintest/tagtest.go
@@ -0,0 +1,70 @@
+package domaintest
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/DavudSafarli/Critique/pkg/api/domain"
+)
+
+// TestTagRepositoryBehaviour does what its name says
+func TestTagRepositoryBehaviour(t *testing.T, abstractRepo domain.TagRepository) {
+	t.Run("CreateMany and GetAll", func(t *testing.T) {
+		insertedTags := []domain.Tag{
+			{
+				Name: "NewTagName",
+			},
+		}
+		numOfInsertedRows := len(insertedTags)
+		ctx, cancel := context.WithCancel(context.Background())
+		t.Cleanup(cancel)
+
+		insertedTags, err := abstractRepo.CreateMany(ctx, insertedTags)
+		t.Cleanup(func() {
+			abstractRepo.RemoveAll(ctx)
+		})
+		if err != nil {
+			t.Fatalf("TagRepository.CreateMany: Failed to add Tags: %s", err)
+		}
+		selectedTags, err := abstractRepo.GetAll(ctx)
+		if err != nil {
+			t.Fatalf("TagRepository.GetAll: Failed to select all Tags: %s", err)
+		}
+		if len(selectedTags) != numOfInsertedRows {
+			t.Fatalf("TagRepository.GetAll: Didn't return all newly inserted Tags: %s", err)
+		}
+		if reflect.DeepEqual(insertedTags, selectedTags) == false {
+			t.Fatal("Inserted Tags are not the same as selected ones database")
+		}
+	})
+
+	t.Run("RemoveMany", func(t *testing.T) {
+		// arrange
+		insertedTags := []domain.Tag{
+			{Name: "NewTagName1"},
+			{Name: "NewTagName2"},
+			{Name: "NewTagName3"},
+		}
+		ctx, cancel := context.WithCancel(context.Background())
+		t.Cleanup(cancel)
+
+		insertedTags, _ = abstractRepo.CreateMany(ctx, insertedTags)
+
+		// act
+		tagIDs := make([]uint, 0, len(insertedTags))
+		for _, val := range insertedTags {
+			tagIDs = append(tagIDs, val.ID)
+		}
+		err := abstractRepo.RemoveMany(ctx, tagIDs)
+
+		// assert
+		if err != nil {
+			t.Fatalf("TagRepository.RemoveMany: Failed to remove Tags: %s", err)
+		}
+		selectedTags, _ := abstractRepo.GetAll(ctx)
+		if len(selectedTags) != 0 {
+			t.Fatalf("TagRepository.RemoveMany: Did not remove all Tags. Database still contains some: %s", err)
+		}
+	})
+}
