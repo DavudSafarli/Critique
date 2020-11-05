d4033a528ca141e6165ec51437893f47edfdc1ac
DavudSafarli <davudseferli@gmail.com>
2020-11-05 23:25:37 +0400

    test: add mocks for repositories

---
+++ external/repository/mocks/tag_repository.go
@@ -0,0 +1,28 @@
+package mocks
+
+import (
+	"context"
+	"github.com/DavudSafarli/Critique/domain/models"
+	"github.com/stretchr/testify/mock"
+)
+
+// TagRepository is an interfce
+type MockTagRepository struct {
+	mock.Mock
+}
+
+func (m *MockTagRepository) CreateMany(ctx context.Context, tags []models.Tag) ([]models.Tag, error) {
+	args := m.Called(ctx, tags)
+	return args.Get(0).([]models.Tag), args.Error(1)
+}
+
+func (m *MockTagRepository) Get(ctx context.Context) ([]models.Tag, error) {
+	args := m.Called(ctx)
+	return args.Get(0).([]models.Tag), args.Error(1)
+}
+
+func (m *MockTagRepository) RemoveMany(ctx context.Context, tagIDs []uint) error {
+	args := m.Called(ctx, tagIDs)
+	return args.Error(1)
+}
+
